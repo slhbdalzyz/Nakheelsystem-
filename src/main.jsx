5871ce147beacf75df0d7b74edba1b6473680532
slhbdalzyz <126108952+slhbdalzyz@users.noreply.github.com>
2026-07-08 02:27:41 +0200

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import NakheelSystemRoot from "./NakheelSystem.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <NakheelSystemRoot />
-  </React.StrictMode>
-);
